bcdc9d608ee11ca51355c0d250a3d05c1336682e
Kachajal <kodida@gmail.com>
2018-12-23 20:33:37 +0100

    Cleaned up debug code

--- VendorProgression/Main.cs
+++ VendorProgression/Main.cs
@@ -39,58 +39,5 @@ namespace VendorProgression
 
             return true;
         }
-
-    }
-
-
-    [HarmonyPatch(typeof(UnityModManager.UI), "Update")]
-    static class UnityModManager_UI_Update_Patch
-    {
-        static void Postfix(UnityModManager.UI __instance)
-        {
-            if (!Main.enabled)
-                return;
-            try
-            {
-                if (Input.GetKeyUp(KeyCode.H) && (Input.GetKey(KeyCode.LeftControl) ||
-                                                  Input.GetKey(KeyCode.RightControl))
-                                              && Game.Instance.Player != null)
-                {
-                    /*var weps = ResourcesLibrary.GetBlueprints<BlueprintItemWeapon>();
-                    foreach (var wep in weps)
-                    {
-                        if (wep.Category == WeaponCategory.Longbow || wep.Category == WeaponCategory.Shortbow)
-                        {
-                            Game.Instance.Player.Inventory.Add(wep);
-                        }
-                    }*/
-
-
-                    var arms = ResourcesLibrary.GetBlueprints<BlueprintItemShield>();
-                    foreach (var armo in arms)
-                    {
-                        Game.Instance.Player.Inventory.Add(armo);
-                    }
-                }
-                
-                if (Input.GetKeyUp(KeyCode.G) && (Input.GetKey(KeyCode.LeftControl) ||
-                                                  Input.GetKey(KeyCode.RightControl))
-                                              && Game.Instance.Player != null)
-                {
-                    Main.logger.Log("Wearing armor guid: " + Game.Instance.Player.MainCharacter.Value.Body.Armor.Armor.Blueprint.AssetGuid);
-                    Main.logger.Log("Wielding weapon guid: " + Game.Instance.Player.MainCharacter.Value.GetFirstWeapon().Blueprint.AssetGuid);
-                    Main.logger.Log("Items in stash: ");
-                    foreach (var ar in Game.Instance.Player.SharedStash.Items)
-                    {
-                        Main.logger.Log(ar.Blueprint.AssetGuid);
-                    }
-                }
-                
-
-            }
-            catch (Exception)
-            {
-            }
-        }
     }
 }
